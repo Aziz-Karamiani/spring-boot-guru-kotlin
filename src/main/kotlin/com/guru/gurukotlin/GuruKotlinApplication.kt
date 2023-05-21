e0b86a8434b106a78b06cdf35e045f0eccb7c3d0
a.karamiani <aziz.karamiani@gmail.com>
2023-05-21 23:12:44 +0330

    Initial commit

---
+++ src/main/kotlin/com/guru/gurukotlin/GuruKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.guru.gurukotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuruKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<GuruKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/guru/gurukotlin/GuruKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.guru.gurukotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GuruKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
